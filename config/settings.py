fe2f49624645007ae41780209e10bd60a44a4b22
Samandar2005 <86775489+Samandar2005@users.noreply.github.com>
2022-06-19 13:06:13 +0500

    html add extends

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'g1(1@8joz(=)y_mw^9ik4)qt==(qmcq*4&0518_uo6)%gkh=lr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp', '127.0.0.1', 'localhost']
 
 
 # Application definition
